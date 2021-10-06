105e951065f5efba03f2a6c887e05dd3669a76a8
Emilypipp22 <814803@mystma.org>
2021-10-06 09:21:43 -0500

    first try

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+#define function
+def countA(w):
+    count = 0
+    for i in range(0, len(w)):
+        if  w[i] == "a":
+            count = count + 1
+        else:
+            count = count
+    return(count)
+
+print(countA("cat"))
